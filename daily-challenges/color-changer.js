22caab969a8918848f06fdbea0504f9f2b88e1cd
kaywambui-1 <kamauchristine85@gmail.com>
2026-07-19 22:25:29 +0300

    update daily-challenges

--- daily-challenges/color-changer.js
+++ daily-challenges/color-changer.js
@@ -1,1 +1,18 @@
-// Select the button
+console.log("javaScript is working!");
+
+
+const button = document.getElementById("changeColor");
+
+const headings = document.querySelectorAll("h1, h2, h3");
+
+button.addEventListener("click", () => {
+
+    const randomColor = "#" + 
+    Math.floor(Math.random() *
+     16777215).toString(16);
+
+    headings.forEach((heading) => {
+        heading.style.color = randomColor;
+    });
+
+});
