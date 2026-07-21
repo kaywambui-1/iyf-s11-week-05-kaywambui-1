c829c7af0082eb84fccbd1b846e6cb0d0f81ee42
kaywambui-1 <kamauchristine85@gmail.com>
2026-07-21 20:00:40 +0300

    update Image-remover.html Image-remover.js

---
+++ daily-challenges/Image-remover.js
@@ -0,0 +1,7 @@
+const removeBtn = document.getElementById("removeBtn");
+removeBtn.addEventListener ("click", function() {
+    const images = document.querySelectorAll("img";
+        images.forEach(function())
+    )
+}
+)
